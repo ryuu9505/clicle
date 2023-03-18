6effacd5d6af1c05688528fe4c5e3e019576cf4a
ryuu <ryuu.public@gmail.com>
2023-03-18 11:32:31 +0900

    Initialize project

---
+++ src/test/java/com/elcilc/clicle/ClicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elcilc.clicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
