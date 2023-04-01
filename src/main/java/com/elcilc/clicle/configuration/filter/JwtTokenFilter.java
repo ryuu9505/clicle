905252578dc60c06a923b88b9c4fd18ea7c67618
ryuu <ryuu.public@gmail.com>
2023-04-01 12:10:30 +0900

    feat: add security configuration and jwt token logics

---
+++ src/main/java/com/elcilc/clicle/configuration/filter/JwtTokenFilter.java
@@ -0,0 +1,72 @@
+package com.elcilc.clicle.configuration.filter;
+
+import com.elcilc.clicle.model.User;
+import com.elcilc.clicle.service.UserService;
+import com.elcilc.clicle.util.JwtTokenUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final UserService userService;
+
+    private final String secretKey;
+
+    private final static List<String> TOKEN_IN_PARAM_URLS = List.of("/api/v1/users/alarm/subscribe");
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain)
+            throws ServletException, IOException {
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String token;
+        try {
+            if (TOKEN_IN_PARAM_URLS.contains(request.getRequestURI())) {
+                log.info("Request with {} check the query param", request.getRequestURI());
+                token = request.getQueryString().split("=")[1].trim();
+            } else if (header == null || !header.startsWith("Bearer ")) {
+                log.error("Authorization Header does not start with Bearer {}", request.getRequestURI());
+                chain.doFilter(request, response);
+                return;
+            } else {
+                token = header.split(" ")[1].trim();
+            }
+
+            String userName = JwtTokenUtils.getUsername(token, secretKey);
+            User userDetails = userService.loadUserByUsername(userName);
+
+            if (!JwtTokenUtils.validate(token, userDetails.getUsername(), secretKey)) {
+                chain.doFilter(request, response);
+                return;
+            }
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    userDetails, null,
+                    userDetails.getAuthorities()
+            );
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (RuntimeException e) {
+            chain.doFilter(request, response);
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+
+}
+
